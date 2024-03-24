eae546547b12436b6a489b0fad23191cc69e1177
Khang Lam Hoang <llamhoangkhang00000@gmail.com>
2024-03-24 03:22:11 -0700

    Redesigned the Main page now Navigation Page

--- NetGroupProject/navigationPage.cs
+++ NetGroupProject/navigationPage.cs
@@ -38,5 +38,10 @@ namespace NetGroupProject
             loginPage.ShowDialog();
             this.Close();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
