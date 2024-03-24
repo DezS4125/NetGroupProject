95a5b3634a222f62eec57bfd8c0d0844cc6f29f2
Khang Lam Hoang <llamhoangkhang00000@gmail.com>
2024-03-24 03:24:39 -0700

    renamed the buttons

--- NetGroupProject/navigationPage.Designer.cs
+++ NetGroupProject/navigationPage.Designer.cs
@@ -33,11 +33,11 @@
             this.txtLog = new System.Windows.Forms.TextBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
+            this.btnUserManagement = new System.Windows.Forms.Button();
+            this.btnMenuManagement = new System.Windows.Forms.Button();
+            this.btnInvoiceManagement = new System.Windows.Forms.Button();
+            this.btnTableReservation = new System.Windows.Forms.Button();
+            this.btnRevenueStatistic = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.groupBox2.SuspendLayout();
             this.groupBox1.SuspendLayout();
@@ -86,11 +86,11 @@
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.button6);
-            this.groupBox1.Controls.Add(this.button5);
-            this.groupBox1.Controls.Add(this.button4);
-            this.groupBox1.Controls.Add(this.button3);
-            this.groupBox1.Controls.Add(this.button2);
-            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.btnRevenueStatistic);
+            this.groupBox1.Controls.Add(this.btnTableReservation);
+            this.groupBox1.Controls.Add(this.btnInvoiceManagement);
+            this.groupBox1.Controls.Add(this.btnMenuManagement);
+            this.groupBox1.Controls.Add(this.btnUserManagement);
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.Location = new System.Drawing.Point(0, 0);
@@ -101,71 +101,73 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Navigation Bar";
             // 
-            // button1
+            // btnUserManagement
             // 
-            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(3, 72);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(155, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "User Management";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnUserManagement.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnUserManagement.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUserManagement.Location = new System.Drawing.Point(3, 72);
+            this.btnUserManagement.Name = "btnUserManagement";
+            this.btnUserManagement.Size = new System.Drawing.Size(155, 23);
+            this.btnUserManagement.TabIndex = 0;
+            this.btnUserManagement.Text = "User Management";
+            this.btnUserManagement.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnMenuManagement
             // 
-            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(3, 95);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(155, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Menu Management";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnMenuManagement.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnMenuManagement.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMenuManagement.Location = new System.Drawing.Point(3, 95);
+            this.btnMenuManagement.Name = "btnMenuManagement";
+            this.btnMenuManagement.Size = new System.Drawing.Size(155, 23);
+            this.btnMenuManagement.TabIndex = 1;
+            this.btnMenuManagement.Text = "Menu Management";
+            this.btnMenuManagement.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btnInvoiceManagement
             // 
-            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(3, 118);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(155, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Invoice Management";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnInvoiceManagement.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnInvoiceManagement.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInvoiceManagement.Location = new System.Drawing.Point(3, 118);
+            this.btnInvoiceManagement.Name = "btnInvoiceManagement";
+            this.btnInvoiceManagement.Size = new System.Drawing.Size(155, 23);
+            this.btnInvoiceManagement.TabIndex = 2;
+            this.btnInvoiceManagement.Text = "Invoice Management";
+            this.btnInvoiceManagement.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // btnTableReservation
             // 
-            this.button4.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(3, 141);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(155, 23);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "Table Reservation";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btnTableReservation.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnTableReservation.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTableReservation.Location = new System.Drawing.Point(3, 141);
+            this.btnTableReservation.Name = "btnTableReservation";
+            this.btnTableReservation.Size = new System.Drawing.Size(155, 23);
+            this.btnTableReservation.TabIndex = 3;
+            this.btnTableReservation.Text = "Table Reservation";
+            this.btnTableReservation.UseVisualStyleBackColor = true;
             // 
-            // button5
+            // btnRevenueStatistic
             // 
-            this.button5.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(3, 164);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(155, 23);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "Revenue statistic";
-            this.button5.UseVisualStyleBackColor = true;
+            this.btnRevenueStatistic.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnRevenueStatistic.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRevenueStatistic.Location = new System.Drawing.Point(3, 164);
+            this.btnRevenueStatistic.Name = "btnRevenueStatistic";
+            this.btnRevenueStatistic.Size = new System.Drawing.Size(155, 23);
+            this.btnRevenueStatistic.TabIndex = 4;
+            this.btnRevenueStatistic.Text = "Revenue statistic";
+            this.btnRevenueStatistic.UseVisualStyleBackColor = true;
             // 
             // button6
             // 
             this.button6.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.Color.Red;
             this.button6.Location = new System.Drawing.Point(3, 417);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(155, 23);
             this.button6.TabIndex = 5;
             this.button6.Text = "Logout";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.btnLogout_Click);
             // 
             // navigationPage
             // 
@@ -193,10 +195,10 @@
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnRevenueStatistic;
+        private System.Windows.Forms.Button btnTableReservation;
+        private System.Windows.Forms.Button btnInvoiceManagement;
+        private System.Windows.Forms.Button btnMenuManagement;
+        private System.Windows.Forms.Button btnUserManagement;
     }
 }
